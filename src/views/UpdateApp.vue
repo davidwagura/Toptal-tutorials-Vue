0231ddb0309714dfbad0069fdd363f322b6ac4d2
David wanjohi <davidwagura83@gmail.com>
2024-02-21 12:27:20 +0300

    add UpdateApp view

---
+++ src/views/UpdateApp.vue
@@ -0,0 +1,34 @@
+<template>
+
+    {{  update }} 
+    
+    </template>
+    
+    
+    <script>
+        import { ref } from 'vue';
+        import { onMounted } from 'vue';
+        import axios from 'axios'
+    
+    
+        export default {
+            setup() {
+    
+                const update = ref([]);
+    
+                onMounted (() => {
+    
+                    axios.get(`http://127.0.0.1:8000/api/articles`)
+                        .then(response => {
+                            update.value = response.data;
+                        });
+                        
+                })
+    
+    
+                return { update }
+            }
+        }
+    
+    </script>
+    
