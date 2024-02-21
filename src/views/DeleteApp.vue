c6a4d60a6a8852b0728aa52a0ed0fc3bf40a2840
David wanjohi <davidwagura83@gmail.com>
2024-02-21 12:25:19 +0300

    add DeleteApp view

---
+++ src/views/DeleteApp.vue
@@ -0,0 +1,34 @@
+<template>
+
+    {{  Delete }} 
+    
+    </template>
+    
+    
+    <script>
+        import { ref } from 'vue';
+        import { onMounted } from 'vue';
+        import axios from 'axios'
+    
+    
+        export default {
+            setup() {
+    
+                const Delete = ref([]);
+    
+                onMounted (() => {
+    
+                    axios.get(`http://127.0.0.1:8000/api/articles`)
+                        .then(response => {
+                            Delete.value = response.data;
+                        });
+                        
+                })
+    
+    
+                return { Delete }
+            }
+        }
+    
+    </script>
+    
